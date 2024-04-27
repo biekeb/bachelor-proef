82b740567ce81c09d7dc35aedcf83ca9c7cc10ed
biekeb <91184065+biekeb@users.noreply.github.com>
2024-04-27 14:39:41 +0200

    Create TitleScreen.jsx

---
+++ bachelor-endproject/src/pages/TitleScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TitleScreen = () => {
+  return (
+    <div>
+      <h1>noir soir</h1>
+      <button>
+        <a href="/home">continue</a>
+      </button>
+    </div>
+  );
+};
+
+export default TitleScreen;
