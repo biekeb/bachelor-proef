6cd8ebe9a6c515ae05ea3601d3cd2a25dc8213a5
biekeb <91184065+biekeb@users.noreply.github.com>
2024-05-05 19:35:36 +0200

    Style: styling the components

---
+++ bachelor-endproject/src/components/Crosshair.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Crosshair = () => {
+  return (
+    <div className="crosshair">
+      <div className="horizontal-line"></div>
+      <div className="vertical-line"></div>
+    </div>
+  );
+};
+
+export default Crosshair;
---
+++ bachelor-endproject/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Logo from "../styling/images/fingerprint.svg";
+export const Loader = () => {
+  return (
+    <div class="scan">
+      <div class="fingerprint"></div>
+    </div>
+  );
+};
