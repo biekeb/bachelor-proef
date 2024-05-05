b92d3358934032b8fde7792b8b65d71292fe29e0
biekeb <91184065+biekeb@users.noreply.github.com>
2024-05-05 19:39:59 +0200

    Feat: creating the homescreen and title screen

---
+++ bachelor-endproject/src/App.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Sky, PointerLockControls, Html } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import { Ground } from "./components/Ground";
+import { Player } from "./components/Player";
+import Bottle from "./components/model/Bottle";
+import InteractiveCube from "./components/InteractiveCube";
+import { useNavigate } from "react-router-dom";
+import Office from "./components/model/Office";
+import { Perf } from "r3f-perf";
+import { Loader } from "./components/Loader";
+import Crosshair from "./components/Crosshair";
+
+export default function App() {
+  const [loader, setLoader] = useState(true);
+  const [showScreen, setShowScreen] = useState(false); // State to control screen visibility
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoader(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClick = () => {
+    console.log("Button clicked");
+    setShowScreen(true); // Show the screen when the bottle is clicked
+  };
+
+  const handleClose = () => {
+    setShowScreen(false); // Hide the screen when closed
+  };
+
+  return (
+    <>
+      {loader ? (
+        <Loader />
+      ) : (
+        <Canvas
+          shadows
+          gl={{ alpha: false }}
+          camera={{ fov: 45 }}
+          raycaster={{
+            computeOffsets: (e) => ({
+              offsetX: e.target.width / 2,
+              offsetY: e.target.height / 2,
+            }),
+          }}
+        >
+          <Perf position="top-left" />
+          <Sky sunPosition={[100, 20, 100]} />
+          <ambientLight intensity={1} />
+          <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+          <Physics gravity={[0, -30, 0]}>
+            <Ground />
+            <Player />
+            <Bottle onClick={handleClick} />
+            {showScreen && (
+              <Html>
+                <div
+                  style={{
+                    width: "200px",
+                    height: "150px",
+                    backgroundColor: "white",
+                    padding: "20px",
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                  }}
+                >
+                  <p>Content of the small screen</p>
+                  <button onClick={handleClose}>Close</button>
+                </div>
+              </Html>
+            )}
+            <Office
+              position={[1, 5, 0]}
+              rotation={[0, 0, 0]}
+              scale={[3, 3, 3]}
+            />
+          </Physics>
+          <PointerLockControls />
+        </Canvas>
+      )}
+      <Crosshair />
+    </>
+  );
+}
+
+//fix camera op object -> small screen opens up when the objetc is pressed
+
+//open object in a pop up screen when clicked on it
+
+//fix player jump
+
+//safe player position in local storage
+
+//make player be able to sprint
+
+//player running up stairs
+
+//should player be able to jump?
+
+//postprossesing
--- bachelor-endproject/src/pages/HomescreenTest.jsx
+++ bachelor-endproject/src/pages/HomescreenTest.jsx
@@ -1,19 +1,55 @@
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import React, { useState } from "react";
 import Office from "../components/model/Office";
-import { Float, OrbitControls, PresentationControls } from "@react-three/drei";
+import {
+  Float,
+  OrbitControls,
+  PositionalAudio,
+  PresentationControls,
+} from "@react-three/drei";
+import music from "../styling/sounds/menu.mp3";
+import {
+  Bloom,
+  DepthOfField,
+  EffectComposer,
+  Noise,
+  Sepia,
+  ColorAverage,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
 
 const HomescreenTest = () => {
   const [position, setPosition] = useState([-8.6, -5, 55]);
   const [cameraRigEnabled, setCameraRigEnabled] = useState(false);
+  const [showTitleScreen, setShowTitleScreen] = useState(true);
 
   const handleButtonClick = (newPosition) => {
     setCameraRigEnabled(true);
     setPosition(newPosition);
   };
 
+  const playMusic = () => {
+    setShowTitleScreen(false);
+    const audio = new Audio(music);
+    audio.loop = true;
+    audio.play();
+  };
+
   return (
     <div>
+      {showTitleScreen && (
+        <div className="homescreen-start">
+          <button
+            className="homescreen-start-button"
+            onClick={() => {
+              playMusic();
+            }}
+          >
+            <h1>START</h1>
+          </button>
+        </div>
+      )}
+
       <Canvas
         camera={{
           position: [0, 0, 2],
@@ -27,28 +63,47 @@ const HomescreenTest = () => {
           zIndex: "1",
         }}
       >
+        <EffectComposer>
+          <DepthOfField
+            focusDistance={0}
+            focalLength={0.02}
+            bokehScale={2}
+            height={480}
+          />
+          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+          <Noise opacity={0.02} />
+          <ColorAverage
+            blendFunction={BlendFunction.NORMAL} // blend mode
+          />
+        </EffectComposer>
+
         {cameraRigEnabled && <CameraRig position={position} />}
-        <PresentationControls
-          controls={{ pointerLock: false }}
-          enabled={!cameraRigEnabled}
-        >
-          <mesh position={(0, 0, 5)}>
-            <boxGeometry args={[1, 10, 10]} />
-            <meshStandardMaterial color="red" />
-          </mesh>
-          <ambientLight intensity={0.5} />
-          <Office />
-        </PresentationControls>
-        {/* <OrbitControls /> */}
+        <ambientLight color={"red"} intensity={0.5} />
+        <Office />
+        <PositionalAudio url={music} distance={1} loop />
+
+        <OrbitControls />
       </Canvas>
-      <div style={{ zIndex: "99", position: "relative" }}>
-        <h1>Camera Test</h1>
-        <button onClick={() => handleButtonClick([0, 0, 5])}>Camera 1</button>
-        <button onClick={() => handleButtonClick([5, 5, 5])}>Camera 2</button>
+      <div
+        className="homescreen-button"
+        style={{ zIndex: "99", position: "relative" }}
+      >
+        <>
+          <button>
+            <a href="app">play</a>
+          </button>
+          <button onClick={() => handleButtonClick([-5, 0, 0])}>
+            Settings
+          </button>
+          <button onClick={() => handleButtonClick([5, 5, 5])}>
+            How to play
+          </button>
+        </>
       </div>
     </div>
   );
 };
+
 function CameraRig({ position: [x, y, z] }) {
   useFrame((state) => {
     state.camera.position.lerp({ x, y, z }, 0.03, "easeInOut");
@@ -59,8 +114,3 @@ function CameraRig({ position: [x, y, z] }) {
 }
 
 export default HomescreenTest;
-
-//camera 1 = default camera + back button
-//camera 2 = setting menu
-//camera 3 = how to play?
-//(camera 4 = credits)
