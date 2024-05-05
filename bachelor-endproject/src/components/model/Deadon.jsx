2cbc31d4fc5a0c0eb8e7dd485c31723963c3fca1
biekeb <91184065+biekeb@users.noreply.github.com>
2024-05-05 19:32:21 +0200

    Adding 3d models

---
+++ bachelor-endproject/src/components/model/Deadon.jsx
@@ -0,0 +1,85 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export default function Model(props) {
+  const { nodes, materials } = useGLTF(
+    "./assets/deadon/deadon-transformed.glb"
+  );
+  return (
+    <group {...props} dispose={null}>
+      <primitive object={nodes.Root} />
+      <mesh
+        geometry={nodes.Node.geometry}
+        material={materials.PaletteMaterial002}
+        position={[-0.367, 0, -0.827]}
+        scale={0.011}
+      />
+      <mesh
+        geometry={nodes.Blood_2.geometry}
+        material={materials["Atlas.039"]}
+        position={[0, 0, -0.649]}
+        scale={100}
+      />
+      <group rotation={[-Math.PI / 2, 0, 0]} scale={100}>
+        <skinnedMesh
+          geometry={nodes.Suit_Body_1.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Suit_Body_1.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Suit_Body_2.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Suit_Body_2.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Suit_Body_3.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Suit_Body_3.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Suit_Body_4.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Suit_Body_4.skeleton}
+        />
+      </group>
+      <skinnedMesh
+        geometry={nodes.Suit_Feet.geometry}
+        material={materials.PaletteMaterial001}
+        skeleton={nodes.Suit_Feet.skeleton}
+        rotation={[-Math.PI / 2, 0, 0]}
+        scale={100}
+      />
+      <group rotation={[-Math.PI / 2, 0, 0]} scale={100}>
+        <skinnedMesh
+          geometry={nodes.Suit_Head_1.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Suit_Head_1.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Suit_Head_2.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Suit_Head_2.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Suit_Head_3.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Suit_Head_3.skeleton}
+        />
+        <skinnedMesh
+          geometry={nodes.Suit_Head_4.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Suit_Head_4.skeleton}
+        />
+      </group>
+      <skinnedMesh
+        geometry={nodes.Suit_Legs.geometry}
+        material={materials.PaletteMaterial001}
+        skeleton={nodes.Suit_Legs.skeleton}
+        rotation={[-Math.PI / 2, 0, 0]}
+        scale={100}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("./assets/deadon/deadon-transformed.glb");
