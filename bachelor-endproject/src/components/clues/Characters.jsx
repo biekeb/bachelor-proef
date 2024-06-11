cbff73d4c84767fbcbae243b320bc4f23d3bf31f
Bieke <91184065+biekeb@users.noreply.github.com>
2024-06-11 16:18:40 +0200

    Feat: chaning the style of the inventory

--- bachelor-endproject/src/components/clues/Characters.jsx
+++ bachelor-endproject/src/components/clues/Characters.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import CharacterManager from "./CharacterManager";
-import isabelImage from "../../styling/images/isa.png";
+import isabelImage from "../../styling/images/unknown character.png";
 import vincentImage from "../../styling/images/isa.png";
 import anthonImage from "../../styling/images/isa.png";
 
@@ -23,31 +23,36 @@ const Characters = () => {
   return (
     <div className="characters">
       <h2>Characters</h2>
-      <ul>
-        {Object.entries(characters).map(([characterName, characterData]) => (
-          <li key={characterName}>
-            {characterData.found ? (
-              <>
-                <h3>
-                  {characterName.charAt(0).toUpperCase() +
-                    characterName.slice(1)}
-                </h3>
-                <p>{characterData.description}</p>
-                <img src={characterImages[characterName]} alt={characterName} />
-              </>
-            ) : (
-              <>
-                <h3>Undiscovered</h3>
-                <img
-                  style={{ width: "100px", height: "100px" }}
-                  src={isabelImage}
-                  alt="unknown character"
-                />
-              </>
-            )}
-          </li>
-        ))}
-      </ul>
+      <div className="character-div">
+        <ul>
+          {Object.entries(characters).map(([characterName, characterData]) => (
+            <li key={characterName}>
+              {characterData.found ? (
+                <>
+                  <h3>
+                    {characterName.charAt(0).toUpperCase() +
+                      characterName.slice(1)}
+                  </h3>
+                  <p>{characterData.description}</p>
+                  <img
+                    src={characterImages[characterName]}
+                    alt={characterName}
+                  />
+                </>
+              ) : (
+                <>
+                  <h3>Undiscovered</h3>
+                  <img
+                    style={{ width: "100px", height: "100px" }}
+                    src={isabelImage}
+                    alt="unknown character"
+                  />
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
